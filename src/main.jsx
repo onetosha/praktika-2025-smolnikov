cd8c643d7d5eda33dac704118f29b21c4db26256
onetosha <onetosha3@yandex.ru>
2025-12-19 15:08:13 +0500

    gh pages

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter> 
+    <BrowserRouter basename="/praktika-2025-smolnikov"> 
       <AppProvider> 
         <App />
       </AppProvider>
